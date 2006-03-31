910943120d5fd7aca2d7a7e2f07baf61712e0586
Andreas Bolka <andreas.bolka@gmx.net>
2006-03-31 00:20:36 +0000

    - improved permission handling

--- apps/sticky.r
+++ apps/sticky.r
@@ -1,17 +1,23 @@
 ; 2004-05-03	johannes lerch, vdp
 ; 2004-07-17	earl
 ; 2005-08-31	added permission check
+; 2006-03-30	sticky toggle is no longer displayed for users w/ insufficient
+;		permissions
 
 make object! [
     odbname: copy "appdata-sticky"
 
     handle: func [ /local odb ] [
+	if permissions-fail? 'edit snip [
+	    ;; insufficient permissions for changing stickyness: don't even
+	    ;; display the link
+	    return ""
+	]
 	if not attempt [ sticky ] [
 	    return render-stickylink
 	]
 
 	;; toggle stickyness
-	permissions-ok?/redir 'edit snip
 	if not space-exists? odbname [ space-store odbname mold [] ]
 	odb: load space-get odbname
 	either = "true" sticky 
@@ -27,7 +33,7 @@ make object! [
 
 	sticky: find odb snip
 	rejoin [
-	    {<a href="} vanilla-display-url snip 
+	    {<a href="} vanilla-display-url (url-encode snip)
 		{&sticky=} either sticky [ "false" ] [ "true" ]
 		{">} either sticky [ "unstick" ] [ "stick" ] {</a>}
 	]
